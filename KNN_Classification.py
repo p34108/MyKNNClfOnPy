a2826eb45e744b17ca86256a1c9efa328ce415d9
p34108 <127743708+p34108@users.noreply.github.com>
2024-04-29 16:19:51 +0300

    Add files via upload

--- KNN_Classification.py
+++ KNN_Classification.py
@@ -3,12 +3,15 @@ import pandas as pd
 
 
 class MyKNNClf:
-    def __init__(self, k=3, metric='euclidean'):
+    def __init__(self, k=3, metric='euclidean', weight='uniform', number_of_classes=2):
         self.k = k
         self.train_size = None
         self.X, self.y = None, None
         self.size = None
         self.metric = metric
+        self.weight = weight
+        self.number_of_classes = number_of_classes
+        self.Q1 = None
 
     def __str__(self):
         return self.train_size
@@ -42,14 +45,49 @@ class MyKNNClf:
             evD = evD.reset_index(drop=True)
         else:
             evD = pd.DataFrame(1 - ((self.X.reset_index(drop=True) * X).sum(axis=1) / (
-                        np.sqrt((X ** 2).sum(axis=1)) * np.sqrt((self.X.reset_index(drop=True) ** 2).sum(axis=1)))))
+                    np.sqrt((X ** 2).sum(axis=1)) * np.sqrt((self.X.reset_index(drop=True) ** 2).sum(axis=1)))))
             evD = evD.reset_index(drop=True)
         evD.columns = ['bD']
         evD['y'] = self.y.reset_index(drop=True)
-        result = evD.sort_values(by='bD').reset_index(drop=True).iloc[:self.k]['y'].mode()
-        if len(result) == 1:
-            return result[0]
-        return 1
+        if self.weight == 'uniform':
+            result = evD.sort_values(by='bD').reset_index(drop=True).iloc[:self.k]['y'].mode()
+            if len(result) == 1:
+                return result[0]
+            return 1
+        elif self.weight == 'rank':
+            weights_dict = {}
+            test = evD.sort_values(by='bD').reset_index(drop=True).iloc[:self.k]
+            test = test.reset_index()
+            test['index'] = test['index'] + 1
+            for i in range(self.number_of_classes):
+                if len(test[test['y'] == i]) == 0:
+                    continue
+                numerator = (np.vectorize(self.rank_or_distance_averaging)(test[test['y'] == i]['index'])).sum()
+                denominator = (np.vectorize(self.rank_or_distance_averaging)(test['index'])).sum()
+                Q_i = numerator / denominator
+                weights_dict[i] = Q_i
+            return self.find_key_with_max_value(weights_dict)
+        else:
+            weights_dict = {}
+            test = evD.sort_values(by='bD').reset_index(drop=True).iloc[:self.k]
+            test = test.reset_index()
+            test['index'] = test['index'] + 1
+            for i in range(self.number_of_classes):
+                if len(test[test['y'] == i]) == 0:
+                    continue
+                numerator = (np.vectorize(self.rank_or_distance_averaging)(test[test['y'] == i]['bD'])).sum()
+                denominator = (np.vectorize(self.rank_or_distance_averaging)(test['bD'])).sum()
+                Q_i = numerator / denominator
+                weights_dict[i] = Q_i
+            return self.find_key_with_max_value(weights_dict)
+
+    def rank_or_distance_averaging(self, stroka):
+        stroka = (1 / stroka)
+        return stroka
+
+    def find_key_with_max_value(self, di):
+        max_key = max(di, key=di.get)
+        return max_key
 
     def proba_calculating_the_distance(self, value):
         data = {f'col{i}': val for i, val in enumerate(list(value))}
@@ -66,7 +104,7 @@ class MyKNNClf:
             evD = evD.reset_index(drop=True)
         else:
             evD = pd.DataFrame(1 - ((self.X.reset_index(drop=True) * X).sum(axis=1) / (
-                        np.sqrt((X ** 2).sum(axis=1)) * np.sqrt((self.X.reset_index(drop=True) ** 2).sum(axis=1)))))
+                    np.sqrt((X ** 2).sum(axis=1)) * np.sqrt((self.X.reset_index(drop=True) ** 2).sum(axis=1)))))
             evD = evD.reset_index(drop=True)
         evD.columns = ['bD']
         evD['y'] = self.y.reset_index(drop=True)
